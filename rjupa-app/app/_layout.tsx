a5a37ec457b70167c112d06834d772bd0dfd4e7c
Benediktedybsjord <benediktedybsjord@outlook.com>
2026-01-12 12:53:16 +0100

    Created an example folder structure, babel.config.ts, tailwindconfig, metro.config etc

--- rjupa-app/app/_layout.tsx
+++ rjupa-app/app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import { Stack } from "expo-router"; 
+import { StatusBar } from 'expo-status-bar'
+import "../global.css";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <>
+    <StatusBar style="auto"/>
+    <Stack screenOptions={{ headerStyle: {backgroundColor: '#004aad'}, headerTintColor: '#fff', headerTitleStyle: {fontWeight: 'bold'} }}/>
+    </>
+  )
 }
